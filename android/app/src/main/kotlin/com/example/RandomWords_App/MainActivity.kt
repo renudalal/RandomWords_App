b4405c0f367785fc35346de18b8e729a5fa4c99d
renudalal <renudalal89@gmail.com>
2020-11-03 17:51:03 -0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/RandomWords_App/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.RandomWords_App
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyHomePage());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyHomePage(),
+//     );
+//   }
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  String wordPair = WordPair.random().asPascalCase;
+
+  void _incrementCounter() {
+    setState(() {
+      wordPair = WordPair.random().asPascalCase;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Random words App'), centerTitle: true),
+      body: Center(
+        child: Text(
+          wordPair,
+          style: TextStyle(
+              fontSize: 24,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
